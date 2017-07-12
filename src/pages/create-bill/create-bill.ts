ab3b1dd018b60678badc9fa450b575bf63d90c04
yass <yass@gmail.com>
2017-07-12 13:34:09 +0200

    create bill finished a nd tested in browser

--- src/pages/create-bill/create-bill.ts
+++ src/pages/create-bill/create-bill.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController } from 'ionic-angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { BillProvider } from '../../providers/bill/bill';
+
 
 /**
  * Generated class for the CreateBillPage page.
@@ -14,11 +17,31 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class CreateBillPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+public newBillForm: FormGroup;
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad CreateBillPage');
-  }
-
+constructor(public navCtrl: NavController, public formBuilder: FormBuilder,
+public billProvider: BillProvider) {
+this.newBillForm = formBuilder.group({
+name: ['', Validators.required],
+amount: ['', Validators.required],
+dueDate: ['', Validators.required]
+});
 }
+
+
+createBill():void {
+if (!this.newBillForm.valid){
+console.log(this.newBillForm.value);
+} else {
+this.billProvider.createBill(this.newBillForm.value.name,
+this.newBillForm.value.amount,this.newBillForm.value.dueDate)
+.then( () => {
+this.navCtrl.pop();
+}, error => {
+console.log(error);
+});
+}
+}
+}
+
+
