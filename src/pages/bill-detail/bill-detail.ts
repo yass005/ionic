0532257efd4416e32b765d33e2f863d7f72f58e9
yass <yass@gmail.com>
2017-07-12 15:14:50 +0200

    finishing bill detail

--- src/pages/bill-detail/bill-detail.ts
+++ src/pages/bill-detail/bill-detail.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController, ActionSheetController, Platform } from 'ionic-angular';
+import { BillProvider } from '../../providers/bill/bill';
 
 /**
  * Generated class for the BillDetailPage page.
@@ -14,11 +15,50 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class BillDetailPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+public bill: any;
+constructor(public navCtrl: NavController, public navParams: NavParams,
+public actionCtrl: ActionSheetController, public platform: Platform,
+public alertCtrl: AlertController, public billProvider: BillProvider) {}
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad BillDetailPage');
   }
-
+ionViewDidEnter(){
+this.billProvider.getBill(this.navParams.get("billId"))
+.subscribe( billSnap => {
+this.bill = billSnap;
+});
+}
+showOptions(billId): void{
+const action = this.actionCtrl.create({
+title: 'Modify your bill',
+buttons: [
+{
+text: 'Delete',
+role: 'destructive',
+icon: !this.platform.is('ios') ? 'trash' : null,
+handler: () => {
+this.billProvider.removeBill(billId)
+.then( () => { this.navCtrl.pop(); });
+}
+},
+{
+text: 'Mark as Paid!',
+icon: !this.platform.is('ios') ? 'checkmark' : null,
+handler: () => {
+this.billProvider.payBill(billId);
+}
+},
+{
+text: 'Cancel',
+role: 'cancel',
+icon: !this.platform.is('ios') ? 'close' : null,
+handler: () => {
+console.log('Cancel clicked');
+}
+}
+]
+});
+action.present();
+}
 }
